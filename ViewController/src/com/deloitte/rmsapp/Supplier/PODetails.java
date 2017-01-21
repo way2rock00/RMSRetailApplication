7d18668728c20349f8d83a334c0547cbb0fdc2cb
aknayak <aknayak@USMUMAKNAYAK2.us.deloitte.com>
2017-01-21 14:00:45 +0530

    Changes for Buyer Screens

--- ViewController/src/com/deloitte/rmsapp/Supplier/PODetails.java
+++ ViewController/src/com/deloitte/rmsapp/Supplier/PODetails.java
@@ -18,6 +18,11 @@ public class PODetails {
     private String headerReason;
     private String edit_flag;
     private String approval_flag;
+    private String notBeforeDate;
+    private String supplierSiteName;
+    private String supplierName;
+    private String buyerNum;
+    private String supplierNum;
 
     public void setEdit_flag(String edit_flag) {
         this.edit_flag = edit_flag;
@@ -126,6 +131,47 @@ public class PODetails {
     }
 
 
+    public void setNotBeforeDate(String notBeforeDate) {
+        this.notBeforeDate = notBeforeDate;
+    }
+
+    public String getNotBeforeDate() {
+        return notBeforeDate;
+    }
+
+    public void setSupplierSiteName(String supplierSiteName) {
+        this.supplierSiteName = supplierSiteName;
+    }
+
+    public String getSupplierSiteName() {
+        return supplierSiteName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setBuyerNum(String buyerNum) {
+        this.buyerNum = buyerNum;
+    }
+
+    public String getBuyerNum() {
+        return buyerNum;
+    }
+
+    public void setSupplierNum(String supplierNum) {
+        this.supplierNum = supplierNum;
+    }
+
+    public String getSupplierNum() {
+        return supplierNum;
+    }
+
+
     public PODetails (String recordId
                     ,String poNumber
                     ,String orderType
@@ -138,6 +184,11 @@ public class PODetails {
                     ,String headerReason
                       ,String edit_flag
                       ,String approval_flag
+                      ,String notBeforeDate
+                      ,String supplierSiteName
+                      ,String supplierName
+                      ,String buyerNum
+                      ,String supplierNum
                     ,POLine[] poLines
                       )
     {
@@ -153,6 +204,11 @@ public class PODetails {
         this.headerReason = headerReason;
         this.edit_flag = edit_flag;
         this.approval_flag = approval_flag;
+        this.notBeforeDate = notBeforeDate;
+        this.supplierSiteName = supplierSiteName;
+        this.supplierName = supplierName;
+        this.buyerNum = buyerNum;
+        this.supplierNum = supplierNum;
         this.poLines = poLines;
     }
 }

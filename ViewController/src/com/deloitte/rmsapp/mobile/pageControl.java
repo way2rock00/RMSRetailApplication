c05389728f4039f6740d7eb32db73989c3c52be3
aknayak <aknayak@USMUMAKNAYAK2.us.deloitte.com>
2017-01-21 14:04:33 +0530

    Changes for Buyer Screens and notification changes

--- ViewController/src/com/deloitte/rmsapp/mobile/pageControl.java
+++ ViewController/src/com/deloitte/rmsapp/mobile/pageControl.java
@@ -1,10 +1,15 @@
 package com.deloitte.rmsapp.mobile;
 
+import com.deloitte.rmsapp.utility.RestURIs;
+
+import com.deloitte.rmsapp.utility.ServiceManager;
+
 import oracle.adfmf.amx.event.ActionEvent;
 import oracle.adfmf.amx.event.ValueChangeEvent;
 import oracle.adfmf.framework.api.AdfmfContainerUtilities;
 import oracle.adfmf.framework.api.AdfmfJavaUtilities;
 import oracle.adfmf.framework.exception.AdfException;
+import oracle.adfmf.json.JSONObject;
 
 public class pageControl {
     private boolean validated = false;
@@ -13,15 +18,71 @@ public class pageControl {
 
     public void LoginIn_buttonClick(ActionEvent actionEvent) {
         //Supplier Check
+        String strReturnStatus = "E";
         System.out.println("LoginIn_buttonClick start");
+        
         String userName = AdfmfJavaUtilities.getELValue("#{pageFlowScope.userName}").toString();
         String password = AdfmfJavaUtilities.getELValue("#{pageFlowScope.password}").toString();
+        String loginType = AdfmfJavaUtilities.getELValue("#{pageFlowScope.loginType}")==null?"BUYER":(String)AdfmfJavaUtilities.getELValue("#{pageFlowScope.loginType}");
+        String deviceId = (String)AdfmfJavaUtilities.getELValue("#{applicationScope.deviceToken}");
+        String appId = AdfmfJavaUtilities.getELValue("#{applicationScope.configuration.adfAppUID}").toString();
+        String strloginNumber = "";
         System.out.println("user:" + userName);
         System.out.println("password:" + password);
+        String strDebug = "S:"+userName+":"+password+":"+loginType+":"+deviceId+":"+appId;
         this.validated = false;
         if (userName == null || password == null) {
 
         }
+        String url = RestURIs.getLoginURL(userName, password,deviceId , loginType,appId );
+        strDebug = strDebug +"s1:"+url;
+        ServiceManager serviceManager = new ServiceManager();
+        System.out.println("po status by header url:" + url);
+        String jsonArrayAsString = serviceManager.invokeREAD(url);
+        strDebug = strDebug +"s2:"+jsonArrayAsString;
+            try{
+                JSONObject jsonObject = new JSONObject(jsonArrayAsString);
+                if(jsonObject != null){
+                    
+                    if(jsonObject.getString("X_RETURN_STATUS") != null){
+                        strReturnStatus = jsonObject.getString("X_RETURN_STATUS");
+                    }
+                    if(jsonObject.getString("X_ENTITY_NUM") != null){
+                        strloginNumber = jsonObject.getString("X_ENTITY_NUM");
+                    }
+                }
+            }
+            catch(Exception e){
+                strDebug = strDebug +"Exception:"+e.getMessage();
+            }
+        strDebug = strDebug + ":s3:" + strReturnStatus + ":s3:" + strloginNumber;
+        //strReturnStatus = "S";
+        if("E".equalsIgnoreCase(strReturnStatus)){
+            this.validated = false;
+            AdfmfJavaUtilities.setELValue("#{pageFlowScope.LoginMessage}", "Invalid Credentials Provided.");
+        }else{
+            this.validated = true;
+            AdfmfJavaUtilities.setELValue("#{pageFlowScope.LoginMessage}", null);
+            AdfmfJavaUtilities.setELValue("#{pageFlowScope.loginType}", loginType);
+            AdfmfJavaUtilities.setELValue("#{applicationScope.loginType}", loginType);
+            AdfmfJavaUtilities.setELValue("#{pageFlowScope.showExpand}", "true");
+            AdfmfJavaUtilities.setELValue("#{applicationScope.userName}", userName);
+            AdfmfJavaUtilities.setELValue("#{applicationScope.appId}", appId);
+            if("BUYER".equalsIgnoreCase(loginType)){
+                AdfmfJavaUtilities.setELValue("#{pageFlowScope.loginBuyer}",strloginNumber );//305
+                AdfmfJavaUtilities.setELValue("#{applicationScope.loginBuyer}", strloginNumber);
+                AdfmfJavaUtilities.setELValue("#{pageFlowScope.searchBuyer}", strloginNumber);              
+                AdfmfJavaUtilities.setELValue("#{applicationScope.searchBuyer}", strloginNumber);              
+            }
+            else if("SUPPLIER".equalsIgnoreCase(loginType)){
+                AdfmfJavaUtilities.setELValue("#{pageFlowScope.loginSuppier}", strloginNumber);//2500
+                AdfmfJavaUtilities.setELValue("#{applicationScope.loginSuppier}", strloginNumber);
+                AdfmfJavaUtilities.setELValue("#{pageFlowScope.searchSupplier}", strloginNumber); 
+                AdfmfJavaUtilities.setELValue("#{applicationScope.searchSupplier}", strloginNumber); 
+            }
+        }
+        AdfmfJavaUtilities.setELValue("#{applicationScope.strLoginDebug}", strDebug);
+        /*
         if (userName.equals("Sup01") && password.equals("Sup123")) {
             System.out.println("valid:");
             this.validated = true;
@@ -57,7 +118,7 @@ public class pageControl {
             
         } else {
             AdfmfJavaUtilities.setELValue("#{pageFlowScope.LoginMessage}", "Invalid Credentials Provided.");
-        }
+        }*/
         System.out.println("LoginIn_buttonClick end");
     }
 
